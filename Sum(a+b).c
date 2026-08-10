06e883ac179487dc6f247cd97450a7137998eca6
Srinjan2006 <sarkarsrinjan5@gmail.com>
2026-08-10 21:38:31 +0530

    Add files via upload

---
+++ Sum(a+b).c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	int c,d;
+	printf("Enter two number");
+	scanf("%d %d",&c,&d);
+	printf("The Addition of c and d is %d",c+d);
+	return 0;
+}
---
+++ Sumc=(a+b).c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("Enter two number");
+	scanf("%d %d",&a,&b);
+	c=a+b;
+	printf("The Addition of a and b is %d",c);
+	return 0;
+}
