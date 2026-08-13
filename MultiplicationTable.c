0f5941d84d5378adcaedb442a8c3c87b97648c6a
Srinjan2006 <sarkarsrinjan5@gmail.com>
2026-08-13 21:04:11 +0530

    Refactor multiplication output format in C program

--- MultiplicationTable.c
+++ MultiplicationTable.c
@@ -4,15 +4,15 @@ int main()
 	int n;
 	printf("The value of n is");
 	scanf("%d",&n);
-	printf("The Multiplication of %d with 1 is %d\n",n*1);
-	printf("The Multiplication of %d with 2 is %d\n",n*2);
-	printf("The Multiplication of %d with 3 is %d\n",n*3);
-	printf("The Multiplication of %d with 4 is %d\n",n*4);
-	printf("The Multiplication of %d with 5 is %d\n",n*5);
-	printf("The Multiplication of %d with 6 is %d\n",n*6);
-	printf("The Multiplication of %d with 7 is %d\n",n*7);
-	printf("The Multiplication of %d with 8 is %d\n",n*8);
-	printf("The Multiplication of %d with 9 is %d\n",n*9);
-	printf("The Multiplication of %d with 10 is %d\n",n*10);
+	printf("%d*1=%d\n",n,n*1);
+	printf("%d*2=%d\n",n,n*2);
+	printf("%d*3=%d\n",n,n*3);
+	printf("%d*4=%d\n",n,n*4);
+	printf("%d*5=%d\n",n,n*5);
+	printf("%d*6=%d\n",n,n*6);
+	printf("%d*7=%d\n",n,n*7);
+	printf("%d*8=%d\n",n,n*8);
+	printf("%d*9=%d\n",n,n*9);
+	printf("%d*10=%d\n",n,n*10);
 	return 0;
 }
