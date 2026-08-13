b7be24ad45282b85bd81f3e8212bec0b868e4456
Srinjan2006 <sarkarsrinjan5@gmail.com>
2026-08-13 20:43:22 +0530

    Fix multiplication output formatting in MultiplicationTable.c

--- MultiplicationTable.c
+++ MultiplicationTable.c
@@ -4,15 +4,15 @@ int main()
 	int n;
 	printf("The value of n is");
 	scanf("%d",&n);
-	printf("The Multiplication of n with 1 is %d\n",n*1);
-	printf("The Multiplication of n with 2 is %d\n",n*2);
-	printf("The Multiplication of n with 3 is %d\n",n*3);
-	printf("The Multiplication of n with 4 is %d\n",n*4);
-	printf("The Multiplication of n with 5 is %d\n",n*5);
-	printf("The Multiplication of n with 6 is %d\n",n*6);
-	printf("The Multiplication of n with 7 is %d\n",n*7);
-	printf("The Multiplication of n with 8 is %d\n",n*8);
-	printf("The Multiplication of n with 9 is %d\n",n*9);
-	printf("The Multiplication of n with 10 is %d\n",n*10);
+	printf("The Multiplication of %d with 1 is %d\n",n*1);
+	printf("The Multiplication of %d with 2 is %d\n",n*2);
+	printf("The Multiplication of %d with 3 is %d\n",n*3);
+	printf("The Multiplication of %d with 4 is %d\n",n*4);
+	printf("The Multiplication of %d with 5 is %d\n",n*5);
+	printf("The Multiplication of %d with 6 is %d\n",n*6);
+	printf("The Multiplication of %d with 7 is %d\n",n*7);
+	printf("The Multiplication of %d with 8 is %d\n",n*8);
+	printf("The Multiplication of %d with 9 is %d\n",n*9);
+	printf("The Multiplication of %d with 10 is %d\n",n*10);
 	return 0;
 }
