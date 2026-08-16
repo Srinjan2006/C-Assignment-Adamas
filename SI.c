b40b981f0ac270939ac101f5a2ae33bb54485c8c
Srinjan2006 <sarkarsrinjan5@gmail.com>
2026-08-16 14:05:50 +0530

    Update prompt text for user input in SI calculation for wrong term for 'p' here

--- SI.c
+++ SI.c
@@ -2,7 +2,7 @@
 int main()
 {
 	float p,r,t,SI;
-	printf("Enter Price,Rate and Time");
+	printf("Enter Principal,Rate and Time");
 	scanf("%f %f %f",&p,&r,&t);
 	SI=(p*r*t)/100;
 	printf("The Simple Interest is %f",SI);
